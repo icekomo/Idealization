3a2076e9c738b030d24fd1a146fb5982238a2d06
Josh <joshGdovindesigns.com>
2021-12-31 00:18:02 -0500

    fixed sun

--- dev/js/darkMode.js
+++ dev/js/darkMode.js
@@ -5,4 +5,5 @@ export let darkModeTL = gsap.timeline({paused:true});
 darkModeTL.to(".white-bg",{duration:0.25, backgroundColor:"#0D242B"},"change")
 .to(".blue-txt",{duration:0.25, color:"#fff"},"change")
 .to(".blue-stroke",{duration:0.25, stroke:"#fff"},"change")
-.to(".blue-fill",{duration:0.25, fill:"#fff"},"change");
+.to(".blue-fill",{duration:0.25, fill:"#fff"},"change")
+.to(".white-fill",{duration:0.25, fill:"#0D242B"},"change");
