d88304ac0573507a5d17c405608af9f6c676b2e4
Josh <joshGdovindesigns.com>
2024-02-14 14:40:07 -0500

    working on colors

--- dev/js/darkMode.js
+++ dev/js/darkMode.js
@@ -1,9 +1,13 @@
 import { gsap } from "gsap";
 
+let white = "#fff";
+let darkBlue = "#0D242B";
+
 
 export let darkModeTL = gsap.timeline({paused:true});
-darkModeTL.to(".white-bg",{duration:0.25, backgroundColor:"#0D242B"},"change")
-.to(".blue-txt",{duration:0.25, color:"#fff"},"change")
-.to(".blue-stroke",{duration:0.25, stroke:"#fff"},"change")
-.to(".blue-fill",{duration:0.25, fill:"#fff"},"change")
-.to(".white-fill",{duration:0.25, fill:"#0D242B"},"change");
+darkModeTL.to(".white-bg",{duration:0.25, backgroundColor: darkBlue},"change")
+.to(".blue-txt",{duration:0.25, color:white},"change")
+.to(".blue-txt",{duration:0.25, color:white},"change")
+.to(".blue-stroke",{duration:0.25, stroke:white},"change")
+.to(".blue-fill",{duration:0.25, fill:white},"change")
+.to(".white-fill",{duration:0.25, fill: darkBlue},"change");
