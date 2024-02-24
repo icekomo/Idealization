268022732b958b6719955587b58ab8562b9b3be3
Josh <joshGdovindesigns.com>
2024-02-23 20:00:49 -0500

    working

--- dev/js/darkMode.js
+++ dev/js/darkMode.js
@@ -5,6 +5,9 @@ let darkBlue = "#0D242B";
 let cardBlue = "#143641";
 let cardCircle = "#1D5060";
 
+let nightTimeRendering = document.querySelector("#night-time");
+gsap.set(nightTimeRendering,{alpha:0});
+
 
 export let darkModeTL = gsap.timeline({paused:true});
 darkModeTL.to(".white-bg",{duration:0.25, backgroundColor: darkBlue},"change")
@@ -14,7 +17,8 @@ darkModeTL.to(".white-bg",{duration:0.25, backgroundColor: darkBlue},"change")
 .to(".blue-fill",{duration:0.25, fill:white},"change")
 // .to(".blue-fill",{duration:0.25, fill:white},"change")
 .to(".card-gray",{duration:0.25, backgroundColor: cardBlue},"change")
-.to(".card-circle",{duration:0.25, backgroundColor: cardCircle},"change");
+.to(".card-circle",{duration:0.25, backgroundColor: cardCircle},"change")
+.to(nightTimeRendering,{duration:0.25, alpha:1},"change");
 
 
 
